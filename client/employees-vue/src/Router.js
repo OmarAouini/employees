25baa365d5d4238437c76bc348e1856df951c4f1
OmarAouini <aouini.omar93@gmail.com>
2020-11-13 17:07:00 +0100

    cleanup folder

---
+++ client/employees-vue/src/Router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+   /* {
+      path: "/",
+      alias: "/index",
+      name: "index",
+      component: () => import("./App.vue")
+    },
+    {
+      path: "/employees",
+      name: "employees",
+      component: () => import("./components/Employees")
+    },
+    {
+      path: "/employees/add",
+      name: "add",
+      component: () => import("./components/AddEmployee")
+    }
+    */
+  ]
+});
